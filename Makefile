493fa052b2537967c8a148ffe5bee8c257b7b250
Eric Schulte <schulte.eric@gmail.com>
2011-06-26 16:36:20 -0700

    explicitly loading up all requirements -- make test now output desired results

--- Makefile
+++ Makefile
@@ -5,13 +5,13 @@ LD_LIBRARY_PATH=$(HERE)
 all: elf.c
 
 libelf.so: elf.lisp ecl-build.lisp
-	ecl -shell ecl-build.lisp
+	ecl -norc -shell ecl-build.lisp
 
 run-test: run-test.c libelf.so
 	gcc -o run-test run-test.c -L/usr/local/lib -L. -lelf -lecl
 
 test: run-test
-	./run-test
+	./run-test|egrep -v "^;;;"
 
 clean:
 	rm -f *.fas *.fasl *.lx32fsl *.lx64fsl \
--- requirements.lisp
+++ requirements.lisp
@@ -1,4 +1,3 @@
-;; ecl script to compile a C file
 (let ((quicklisp-init (merge-pathnames ".quicklisp.ecl/setup.lisp"
                                        (user-homedir-pathname))))
   (when (probe-file quicklisp-init)
@@ -11,11 +10,3 @@
                :cl-ppcre
                :diff))
   (require lib))
-(require :cmp)
-(require :elf)
-(setf c::*compile-in-constants* t)
-(c:build-shared-library "elf"
- :lisp-files (list (compile-file "requirements" :system-p t :verbose nil)
-                   (compile-file "package" :system-p t :verbose nil)
-                   (compile-file "util" :system-p t :verbose nil)
-                   (compile-file "elf" :system-p t :verbose nil)))
